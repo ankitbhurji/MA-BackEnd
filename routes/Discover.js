4b3eb2f625ea1fb9891075b475cc90212fc41be4
ankitbhurji <bhurji204@gmail.com>
2022-12-28 17:07:13 +0530

    added filter to discover api

--- routes/Discover.js
+++ routes/Discover.js
@@ -7,10 +7,26 @@ route.get("/categories", async (req, res)=>{
     res.send(data); 
 });
 
-
 route.get("/:ctg", async (req, res)=>{
 
-    const getData = await Gallery.find({Category: {$in: [req.params.ctg]}}).limit(4)
+    var filter = {}
+    if(req.query.filterByLike=="true"){
+        filter = {Like: 1}
+    }
+
+
+    CreateKey = 1;  
+    if(req.query.filterByDate){
+        if(req.query.filterByDate=="asd"){
+            CreateKey = 1;
+        }else if(req.query.filterByDate=="desd"){
+            CreateKey = -1
+        }
+    }
+
+   
+
+    const getData = await Gallery.find({Category: {$in: [req.params.ctg]}, ...filter}).sort({CreatedAt: CreateKey}).limit(4)
     res.send(getData);      
 })
 
